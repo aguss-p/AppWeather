e77b9cc9535a28aeb9c49d345abbed87eb8bc79e
aguss-p <agustinspataro98@gmail.com>
2021-04-23 18:08:30 -0300

    commit inicial

---
+++ src/hooks/useCityPage.jsx
@@ -0,0 +1,42 @@
+import { useState, useEffect} from 'react' //,useDebugValue si quiero importarlo
+import axios from 'axios'
+import { useParams } from 'react-router-dom'
+import { getForecastUrl } from './../utils/urls'
+import getChartData from './../utils/transform/getChartData'
+import getForecastItemList from './../utils/transform/getForecastItemList'
+
+
+const useCityPage = () =>{
+    const [chartData, setChartData] = useState(null)
+    const [forecastItemList, setForecastItemList] = useState(null)
+
+    const { city, countryCode } = useParams()
+    // useDebugValue(`useCityPage ${city}`)//muestra un valor en la consola (no sobreutilizarlo)
+    useEffect(() => {
+        const getForecast = async () => {           
+            const url = getForecastUrl({city, countryCode})
+
+            try {
+                const { data } = await axios.get(url)
+
+                const dataAux = getChartData(data)
+
+                setChartData(dataAux)
+
+                const forecastItemListAux = getForecastItemList(data)
+                //
+
+                //
+                setForecastItemList(forecastItemListAux)            
+            } catch (error) {
+                console.log(error)            
+            }
+        }
+
+        getForecast()
+
+    }, [city, countryCode])
+
+    return { city, countryCode, chartData, forecastItemList }
+}
+export default useCityPage
