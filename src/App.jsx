618922a11aba59ffa7de30f357b080acd30ae179
aguss-p <agustinspataro98@gmail.com>
2021-04-27 08:15:19 -0300

    Revert "Revert "comentario""
    This reverts commit 14559ca7c5f14f6897ae71a1ebef78ffe692f99e.

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,14 @@ import CityPage from './pages/CityPage'
 import NotFoundPage from './pages/NotFoundPage'
 
 const App = () => {
+    const initialvalue={
+
+    }
+    const reducer = () => {
+
+    }
+    const [state, dispatch] = useReducer(reducer, initialValue)
+/*
     const [allWeather, setAllWeather] = useState({})
     const [allChartData, setAllChartData] = useState({})
     const [allForecastItemList, setForecastItemList] = useState({})
@@ -42,7 +50,7 @@ const App = () => {
             allForecastItemList
         }
     ), [allWeather,allChartData,allForecastItemList])
-
+*/
 // comentario
     return (
         <Router>
